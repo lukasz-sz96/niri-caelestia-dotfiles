ece6b0905f11dbd75f7c8a6baa87529db3005008
Lukasz <lukasz.szczesny96@gmail.com>
2026-05-19 12:34:35 +0200

    Support lazygit for bare dotfiles

--- .config/fish/functions/dots.fish
+++ .config/fish/functions/dots.fish
@@ -13,6 +13,9 @@ function dots --description "Manage bare dotfiles repo"
             case git-dir
                 printf '%s\n' "$dotfiles_git"
                 return
+            case lazygit lg
+                command lazygit --git-dir="$dotfiles_git" --work-tree="$dotfiles_work_tree" $argv[2..-1]
+                return
         end
     end
 
